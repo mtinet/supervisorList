455dcbee4119d95fff7308daf455642d8c6cc0c0
Jason Juhyun Kim <mtinet@hanmail.net>
2023-02-21 01:02:55 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const dataUrl = "https://raw.githubusercontent.com/mtinet/supervisorList/main/supervisorList.csv";
+const resultsContainer = document.getElementById("results");
+
+function searchName(name) {
+  if (name === "") {
+    return; // 검색어가 없으면 검색하지 않고 종료
+  }
+  fetch(dataUrl)
+    .then(response => response.text())
+    .then(data => {
+      const rows = data.trim().split("\n").slice(1);
+      const teachers = rows.map(row => row.split(","));
+      const matchingTeachers = teachers.filter(teacher => teacher[2].includes(name));
+      if (matchingTeachers.length > 0) {
+        displayResults(matchingTeachers);
+      } else {
+        displayResults("No results found.");
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function displayResults(teachers) {
+  if (typeof teachers === "string") {
+    resultsContainer.innerHTML = teachers;
+  } else {
+    const html = teachers.map(teacher => `
+      <div class="teacher">
+        <img src="https://raw.githubusercontent.com/mtinet/supervisorList/main/photos/${teacher[0] < 365 ? 0 : teacher[0]}.jpg" width="500px" alt="${teacher[2]}">
+        <div>
+          <h2>${teacher[2]}</h2>
+          <p><strong>Position:</strong> ${teacher[3]}</p>
+          <p><strong>Subject:</strong> ${teacher[4]}</p>
+          <p><strong>Phone:</strong> ${teacher[5]}</p>
+          <p><strong>Email:</strong> ${teacher[6]}</p>
+          <p><strong>Address:</strong> ${teacher[7]}</p>
+        </div>
+      </div>
+    `).join("");
+    resultsContainer.innerHTML = html;
+  }
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  const name = form.elements.name.value;
+  searchName(name);
+});
