be0d85b0a17a2986802df8dc1f08ef6f86173ba9
Jason Juhyun Kim <mtinet@hanmail.net>
2023-02-21 00:55:22 +0900

    Delete photos.js

--- photos.js
+++
@@ -1,5 +0,0 @@
-var order = 660
-
-document.write('<a class="thumbnail" href="https://github.com/mtinet/supervisorList/blob/main/photos/'+ order +'.jpg?raw=true" data-position="left center">');
-
-document.write('<img src="https://github.com/mtinet/supervisorList/blob/main/photos/'+ order +'.jpg?raw=true" alt="" /></a>');
