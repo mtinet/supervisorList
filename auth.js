c4da0413327faafc90260efbcd1c94e8b47b0841
Jason Juhyun Kim <mtinet@hanmail.net>
2023-02-21 23:01:56 +0900

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,41 @@
+const dataUrl = "https://raw.githubusercontent.com/mtinet/supervisorList/main/supervisorList.csv";
+const submitButton = document.querySelector("input[type=submit]");
+
+function authenticateUser(event) {
+  event.preventDefault(); // 폼 기본 동작 취소
+
+  const number = document.getElementById("number").value;
+  const name = document.getElementById("name").value;
+
+  // 검색어가 없으면 검색하지 않고 종료
+  if (number === "" || name === "") {
+    displayMessage("기수와 이름에 알맞은 내용을 입력하세요.");
+    return;
+  }
+
+  fetch(dataUrl)
+    .then(response => response.text())
+    .then(data => {
+      const rows = data.trim().split("\n").slice(1);
+      const teachers = rows.map(row => row.split(","));
+      const matchingTeacher = teachers.find(teacher => teacher[1] === number && teacher[2] === name);
+      if (matchingTeacher) {
+        window.location.href = "indexInner.html";
+      } else {
+        displayMessage("입력하신 정보가 맞지 않습니다.");
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function displayMessage(message) {
+  const messageContainer = document.getElementById("result");
+  messageContainer.innerHTML = message;
+
+  // 3초 후 메시지 초기화
+  setTimeout(() => {
+    messageContainer.innerHTML = "";
+  }, 3000);
+}
+
+submitButton.addEventListener("click", authenticateUser);
